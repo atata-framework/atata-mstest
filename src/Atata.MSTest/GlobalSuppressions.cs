01a23e8f59789880e34be8b63f8d9ed8f8554ad3
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-08-30 15:25:11 +0200

    Add `SuppressMessage` attributes for MSTEST0049

---
+++ src/Atata.MSTest/GlobalSuppressions.cs
@@ -0,0 +1,5 @@
+﻿#pragma warning disable S103 // Lines should not be too long
+
+[assembly: SuppressMessage("Usage", "MSTEST0049:Flow TestContext.CancellationToken to async operations", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.MSTest.AtataTestSuite.SetUpSuiteAtataContext(Microsoft.VisualStudio.TestTools.UnitTesting.TestContext)")]
+[assembly: SuppressMessage("Usage", "MSTEST0049:Flow TestContext.CancellationToken to async operations", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.MSTest.AtataTestSuite.SetUpTestAtataContext")]
+[assembly: SuppressMessage("Usage", "MSTEST0049:Flow TestContext.CancellationToken to async operations", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.MSTest.MSTestGlobalAtataContextSetup.SetUp(System.Type,Microsoft.VisualStudio.TestTools.UnitTesting.TestContext,System.Action{Atata.AtataContextBuilder})")]
--- src/Atata.MSTest/GlobalUsings.cs
+++ src/Atata.MSTest/GlobalUsings.cs
@@ -1,3 +1,3 @@
 ﻿global using System.Collections.Concurrent;
+global using System.Diagnostics.CodeAnalysis;
 global using System.Reflection;
-global using Microsoft.VisualStudio.TestTools.UnitTesting;

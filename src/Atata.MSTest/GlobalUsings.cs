c8a57cc8b1b3f4e170493db4356423be7ce45a03
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-12-27 10:17:25 +0100

    Add `Guard`

--- src/Atata.MSTest/GlobalUsings.cs
+++ src/Atata.MSTest/GlobalUsings.cs
@@ -1,3 +1,6 @@
 ﻿global using System.Collections.Concurrent;
+global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Reflection;
+global using System.Runtime.CompilerServices;
+global using System.Text;
